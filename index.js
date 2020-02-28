c4660a5dc12e5a29a8734984267a2f956fd5b353
GravyHTX <godisgravy@gmail.com>
2020-02-28 00:00:42 -0600

    Basic foundation. more to go...

--- index.js
+++ index.js
@@ -2,33 +2,70 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
-inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
-    name: "username"
+function promptUser() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "git",
+        message: "What is your GitHub username?"
+      },
+      {
+        type: "input",
+        name: "location",
+        message: "Where are you from?"
+      },
+      {
+        type: "input",
+        name: "hobby",
+        message: "What is your favorite hobby?"
+      },
+      {
+        type: "input",
+        name: "food",
+        message: "What is your favorite food?"
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username"
+      },
+      {
+        type: "input",
+        name: "linkedin",
+        message: "Enter your LinkedIn URL."
+      }
+    ]);
+  }
+  
+    function generateHTML(answers) {
+    return `
+    ## ${answers.project}
+    ### by username: ${answers.git}
 
+    ## Description
+    ${answers.description}
 
+    ## Table of Contents
+    ${answers.cotents}
 
-  })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    ## Installation
+    ${answers.install}
 
-    axios.get(queryUrl).then(function(res) {
-      const repoNames = res.data.map(function(repo) {
-        return repo.name;
-      });
+    ## Usage
+    ${answers.install}
 
-    //   const repoNamesStr = repoNames.join("\n");
+    ## Contributing
+    ${answers.install}
 
-    //   fs.writeFile("README.md", repoNamesStr, function(err) {
-    //     if (err) {
-    //       throw err;
-    //     }
+    ## Tests
+    ${answers.install}
 
-        console.log(repo.name);
-      });
-    });
-//   });
+    ## Questions
+    ${answers.install}
+  `;
+
+  
+  }
 
 // * At least one badge
 // * Project title
@@ -40,3 +77,56 @@ inquirer
 // * Contributing
 // * Tests
 // * Questions
+// promptUser()
+//   .then(function(answers) {
+//     const html = generateHTML(answers);
+
+//     return writeFileAsync("index.html", html);
+//   })
+//   .then(function() {
+//     console.log("Successfully wrote to index.html");
+//   })
+//   .catch(function(err) {
+//     console.log(err);
+//   });
+
+
+inquirer
+  .prompt({
+    message: "Enter your GitHub username:",
+    name: "username"
+  })
+  .then(function({ username }) {
+    
+
+    axios.get(queryUrl).then(function(res) {
+      const repoNames = res.data.map(function(repo) {
+        return repo.name;
+      });
+
+      const repoNamesStr = repoNames.join("\n");
+
+      fs.writeFile("repos.txt", repoNamesStr, function(err) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(`Saved ${repoNames.length} repos`);
+      });
+    });
+  });
+
+
+  const queryUrl = `https://api.github.com/users/${answers.username}/repos${answers.project}`;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function(response) {
+
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(response);
